04a6dfaaf854ae99f3e175b49efea3e9176ff184
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2021-12-02 21:30:03 +0600

    tyrty

--- DailyExpenseTracker/Areas/CategoryArea/Controllers/CategoryController.cs
+++ DailyExpenseTracker/Areas/CategoryArea/Controllers/CategoryController.cs
@@ -71,33 +71,10 @@ namespace DailyExpenseTracker.Areas.CategoryArea.Controllers
             
         }
 
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Value>> Get(int id)
-        //{
-        //    var response = await _repository.GetById(id);
-        //    if (response == null) { return NotFound(); }
-        //    return response;
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public async Task Post([FromBody] Value value)
-        //{
-        //    await _repository.Insert(value);
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public async Task Delete(int id)
-        //{
-        //    await _repository.DeleteById(id);
-        //}
+        [HttpPost]
+        public async Task<ActionResult> Delete(int? Id)
+        {
+           return Json(await _categoryService.DeleteById(Id));
+        }
     }
 }
