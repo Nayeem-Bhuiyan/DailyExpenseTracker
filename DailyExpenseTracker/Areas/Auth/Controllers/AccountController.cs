30df9bf06ace467313313e1471a63b10bfd5663a
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2021-12-03 01:07:50 +0600

    fghfghfg

--- DailyExpenseTracker/Areas/Auth/Controllers/AccountController.cs
+++ DailyExpenseTracker/Areas/Auth/Controllers/AccountController.cs
@@ -126,121 +126,11 @@ namespace DailyExpenseTracker.Areas.Auth.Controllers
                 AddErrors(result);
             }
 
-             return RedirectToLocal("/DailyExpenseArea/DailyExpense/Index"); ;
+            return RedirectToAction("Index", "DailyExpense", new { area = "DailyExpenseArea" });
 
         }
 
-       
 
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> UserRoleCreate()
-        {
-            var roles = await _roleManager.Roles.ToListAsync();
-            List<ApplicationRoleViewModel> lstRole = new List<ApplicationRoleViewModel>();
-            foreach (var data in roles)
-            {
-                ApplicationRoleViewModel model = new ApplicationRoleViewModel
-                {
-                    RoleId = data.Id,
-                    RoleName = data.Name
-                };
-                lstRole.Add(model);
-            }
-            ApplicationRoleViewModel viewModel = new ApplicationRoleViewModel
-            {
-                roleViewModels = lstRole
-            };
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UserIdentityRoleCreate([FromForm] ApplicationRoleViewModel model)
-        {
-            
-            if (model.RoleId != null)
-            {
-                var role = await _roleManager.FindByIdAsync(model.RoleId);
-                role.Name = model.RoleName;
-                IdentityResult result = await _roleManager.UpdateAsync(role);
-            }
-            else
-            {
-                var role = new ApplicationRole(model.RoleName);
-                IdentityResult result = await _roleManager.CreateAsync(role);
-            }
-            return RedirectToAction(nameof(UserRoleCreate));
-        }
-        
-      
-       [HttpGet]
-       public async Task<IActionResult> UserRoleAssign()
-        {
-            var roles = await _roleManager.Roles.ToListAsync();
-            List<ApplicationRoleViewModel> lstRole = new List<ApplicationRoleViewModel>();
-            foreach (var data in roles)
-            {
-                ApplicationRoleViewModel modelr = new ApplicationRoleViewModel
-                {
-                    RoleId = data.Id,
-                    RoleName = data.Name
-                };
-                lstRole.Add(modelr);
-
-            }
-            UserListViewModel model = new UserListViewModel
-            {
-                aspNetUsersViewModels = await userInfoes.GetUserInfoList(),
-                userRoles = lstRole,
-           
-            };
-            return View(model);
-        }
-
-        public async Task<IActionResult> DeleteRoles(string id)
-        {
-            try
-            {
-                await userInfoes.DeleteRoleById(id);
-                return Json("Success");
-            }
-            catch
-            {
-                return Json("Roles is Already Assigned Someone!!");
-            }
-        }
-
-
-
-
-        [HttpGet]
-        public async Task<IActionResult> UpdateStatus(string Id, int status)
-        {
-            await userInfoes.UpdateUserStatusByUserIdAndStatus(Id, status);
-            return RedirectToAction(nameof(UserList));
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Delete(string Id)
-        {
-            //string userName = HttpContext.User.Identity.Name;
-             await userInfoes.DeleteUser(Id);
-            return RedirectToAction(nameof(UserList));
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> EditRole([FromForm] ApplicationRoleViewModel model)
-        {
-
-            ApplicationUser user = await _userManager.FindByNameAsync(model.userName);
-
-            if (model.PreRoleId != null)
-            {
-                await _userManager.RemoveFromRoleAsync(user, model.PreRoleId);
-            }
-            await _userManager.AddToRoleAsync(user, model.RoleId);
-            return RedirectToAction(nameof(UserList));
-        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -248,8 +138,8 @@ namespace DailyExpenseTracker.Areas.Auth.Controllers
         {
             await _signInManager.SignOutAsync();
             var ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
-            
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+
+            return RedirectToAction("Login", "Account", new { area = "Auth" });
         }
 
         [HttpPost]
@@ -262,96 +152,13 @@ namespace DailyExpenseTracker.Areas.Auth.Controllers
                 var data = await _userManager.ChangePasswordAsync(await _userManager.FindByNameAsync(HttpContext.User.Identity.Name), model.OldPassword, model.Password);
                 message = data.ToString();
             }
-            return RedirectToAction(nameof(HomeController.Index), "Home", new { Message = message });
+            return RedirectToAction("Login", "Account", new { area = "Auth" });
         }
 
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult ResetPassword()
-        {
-            ResetPasswordViewModel model = new ResetPasswordViewModel
-            {
-                
 
-            };
-            return View(model);
-        }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                   
-                    ApplicationUser user = await _userManager.FindByNameAsync(model.Name);
-                    var result = await _userManager.RemovePasswordAsync(user);
-                    var results = await _userManager.AddPasswordAsync(user, model.Password);
-                    string filter = model.Name;
-                    if (results.Succeeded)
-                    {
-                        TempData["Success"] = "Password Changed Successfully!";
-                    }
-                    else
-                    {
-                        AddErrors(results);
-                    }
 
-                }
-               
 
-                return RedirectToAction("UserList");
-            }
-            catch (Exception ex)
-            {
-                return Json("Try again");
-            }
-        }
-
-        [Authorize(Roles = "Super Admin, Admin Nayeem")]
-        public async Task<IActionResult> UserProxyByAdmin()
-        {
-            string userName = HttpContext.User.Identity.Name;
-            var roles = await _roleManager.Roles.ToListAsync();
-            List<ApplicationRoleViewModel> lstRole = new List<ApplicationRoleViewModel>();
-            foreach (var data in roles)
-            {
-                ApplicationRoleViewModel modelr = new ApplicationRoleViewModel
-                {
-                    RoleId = data.Id,
-                    RoleName = data.Name
-                };
-                lstRole.Add(modelr);
-            }
-            UserListViewModel model = new UserListViewModel
-            {
-                userRoles = lstRole,
-            };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> SwitchedUser(string userId, string securityCode)
-        {
-            string userName = HttpContext.User.Identity.Name;
-            string returnUrl = "/";
-            ApplicationUser user = await _userManager.FindByNameAsync(userId);
-            if (user != null && securityCode == "na.nab")
-            {
-                await _signInManager.SignOutAsync();
-                await _signInManager.SignInAsync(user, isPersistent: false);
-
-                return RedirectToLocal(returnUrl);
-
-            }
-            else
-            {
-                return RedirectToAction(nameof(UserProxyByAdmin));
-            }
-        }
         #region GeneralUser
         [AllowAnonymous]
         [HttpGet]
@@ -385,9 +192,9 @@ namespace DailyExpenseTracker.Areas.Auth.Controllers
 
                 AddErrors(result);
             }
-         
-            return View();
-          
+
+            return RedirectToAction("Index", "DailyExpense", new { area = "DailyExpenseArea" });
+
 
         }
         #endregion
@@ -416,70 +223,6 @@ namespace DailyExpenseTracker.Areas.Auth.Controllers
             return View(model);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> CreateUser(string returnUrl = null)
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-
-            var roles = await _roleManager.Roles.ToListAsync();
-            List<ApplicationRoleViewModel> lstRole = new List<ApplicationRoleViewModel>();
-            foreach (var data in roles)
-            {
-                ApplicationRoleViewModel modelr = new ApplicationRoleViewModel
-                {
-                    RoleId = data.Id,
-                    RoleName = data.Name
-                };
-                lstRole.Add(modelr);
-            }
-            var model = new RegisterViewModel
-            {
-                userRoles = lstRole,
-              
-            };
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateUser(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // string username = HttpContext.User.Identity.Name;
-          
-                var user = new ApplicationUser
-                {
-                    UserName = model.Name,
-                    isActive = 1,
-                    Email = model.Email,
-                    createdAt=DateTime.Now
-                };
-                var result = await _userManager.CreateAsync(user, model.Password);
-
-                AddErrors(result);
-            }
-
-            var roles = await _roleManager.Roles.ToListAsync();
-            List<ApplicationRoleViewModel> lstRole = new List<ApplicationRoleViewModel>();
-            foreach (var data in roles)
-            {
-                ApplicationRoleViewModel modelr = new ApplicationRoleViewModel
-                {
-                    RoleId = data.Id,
-                    RoleName = data.Name
-                };
-                lstRole.Add(modelr);
-            }
-            var formData = new RegisterViewModel
-            {
-                userRoles = lstRole,
-               
-            };
-            return View(formData);
-        }
-
-
         public IActionResult AccessDenied()
         {
             return View();
@@ -500,18 +243,15 @@ namespace DailyExpenseTracker.Areas.Auth.Controllers
         {
             if (Url.IsLocalUrl(returnUrl))
             {
-                //return Redirect(returnUrl);
                 var userId = HttpContext.User.Identity.Name;
-               
-                    return RedirectToAction(nameof(HomeController.Index), "Home");
-                
+                return RedirectToAction("Index", "DailyExpense", new { area = "DailyExpenseArea" });
+
             }
             else
             {
                 var userId = HttpContext.User.Identity.Name;
-                
-                    return RedirectToAction(nameof(HomeController.Index), "Home");
-                
+                return RedirectToAction("Index", "DailyExpense", new { area = "DailyExpenseArea" });
+
             }
         }
 
